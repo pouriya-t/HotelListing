2992c6da4c121d38171c7ae11c829a893375de84
pouriya-t <pouriya.un@gmail.com>
2021-06-26 14:15:54 +0430

    Add project files.

---
+++ HotelListing/Data/DatabaseContext.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Behamas",
+                    ShortName = "BS"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Caymen Islaned",
+                    ShortName = "CI"
+                });
+
+            builder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and spa",
+                    Address = "Nergril",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "George Town",
+                    CountryId = 3,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Address = "Nassua",
+                    CountryId = 2,
+                    Rating = 4
+                });
+        }
+
+        
+    }
+}
